0b2c82e667d314056be648ec6ab04c49b9f0fd98
GYUNGAEEEE <ko2931@naver.com>
2024-05-22 02:47:17 +0900

    여행 계획 짜기 front

--- src/main/java/com/triplan/planner/plan/controller/PlanController.java
+++ src/main/java/com/triplan/planner/plan/controller/PlanController.java
@@ -1,6 +1,8 @@
 package com.triplan.planner.plan.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +10,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/plan")
 public class PlanController {
 
+    @Value("${KAKAO_API_KEY}")
+    String KAKAO_API_KEY;
+
     @GetMapping("/list")
     public String list() {
         return "plan/myPlanList";
+    }
+
+    @GetMapping("/write")
+    public String write(Model model) {
+        model.addAttribute("KAKAO_API_KEY", KAKAO_API_KEY);
+        return "plan/planForm";
     }
 }
