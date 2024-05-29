d2214abd7be2930129a77c17341dac788dc3dbfa
Wendy <wendy_0805@naver.com>
2024-05-29 15:44:19 +0900

    dailyweather, weekTemp 완성!

--- src/main/java/com/triplan/planner/admin/controller/Test.java
+++ src/main/java/com/triplan/planner/admin/controller/Test.java
@@ -1,5 +1,13 @@
 package com.triplan.planner.admin.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class Test {
 
+    @GetMapping("/index")
+    public String main(){
+        return "/main/index";
+    }
 }
