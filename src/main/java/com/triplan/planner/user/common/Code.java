ab3f812cdc1ce1a93e8358139e27a4b8b8f7191b
Moon <82779002+Ramambo@users.noreply.github.com>
2024-05-31 11:59:17 +0900

    Revert "User"

--- src/main/java/com/triplan/planner/user/common/Code.java
+++
@@ -1,6 +0,0 @@
-package com.triplan.planner.user.common;
-
-public interface Code {
-    String name();
-    String getMessage();
-}
--- src/main/java/com/triplan/planner/user/repository/TestMapper.java
+++
@@ -1,8 +0,0 @@
-package com.triplan.planner.user.repository;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface TestMapper {
-    public void test();
-}
